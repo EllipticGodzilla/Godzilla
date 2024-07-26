11203df766ea1fcc4328452d2264799d1a320ff0
EllipticGodzilla <camillo.zamponi@gmail.com>
2024-07-26 15:58:24 +0200

    Revert "bck"
    This reverts commit 0ad5edf460d9f254f999a2987d55cf583e0930c8.

--- src/gui/TempPanel_action.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import java.util.Vector;
-
-public interface TempPanel_action {
-    Vector<String> input = new Vector<>();
-    void success();
-    void fail();
-}
--- src/network/On_arrival.java
+++
@@ -1,5 +0,0 @@
-package network;
-
-public interface On_arrival {
-    void on_arrival(byte conv_code, byte[] msg);
-}
